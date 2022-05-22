0f37927073d47acf056519c8e0ad7d65b812a462
James Bester <jamesthomasbester@gmail.com>
2022-05-22 22:08:19 +1000

    filled out the models, started on the get requests

--- models/Product.js
+++ models/Product.js
@@ -33,8 +33,10 @@ Product.init(
     },
     category_id: {
       type: DataTypes.INTEGER,
-      references: 'Category',
-      referenceskey: 'id'
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
 
   },
